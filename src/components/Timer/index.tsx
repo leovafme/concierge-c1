f2ce364e665755db68b368343f087ab3b2a8528c
lavalencia9029 <lavalencia9029@misena.edu.co>
2020-08-31 18:20:13 -0500

    add duration

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -7,6 +7,7 @@ import './index.css'
 function Timer() {
   const [myTime, setMyTime] = useState('00:00:00')
   const [running, setRunning] = useState(false)
+  const [myTimeElapsed, setMyTimeElapsed] = useState('00:00:00')
 
   /**
    * listen when running changed
@@ -15,9 +16,20 @@ function Timer() {
     let timer:any = null
 
     if (running) {
+      const myTimeInit = new Date()
+
       timer = setInterval(() => {
-        const date = moment().format('HH:mm:ss')
-        setMyTime(`${date}`)
+        const dateInitial = moment(myTimeInit)
+        const dateCurrent = moment()
+
+        setMyTime(`${dateCurrent.format('HH:mm:ss')}`)
+
+        const duration = moment.duration(dateCurrent.diff(dateInitial))
+        const durationHours = Math.floor((duration.asMilliseconds() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const durationMinutes = Math.floor((duration.asMilliseconds() % (1000 * 60 * 60)) / (1000 * 60));
+        const durationSeconds = Math.floor((duration.asMilliseconds() % (1000 * 60)) / 1000);
+
+        setMyTimeElapsed(`${durationHours}:${durationMinutes}:${durationSeconds}`)
       }, 1000)
     } else if (!running) {
       clearInterval(timer)
@@ -45,13 +57,18 @@ function Timer() {
    */
   const cleanTimer = () => {
     setMyTime('00:00:00')
+    setMyTimeElapsed('00:00:00')
     setRunning(false)
   }
 
   return (
     <>
       <Row>
-        <Col span={24} className="timer--container-time">{myTime}</Col>
+        <Col span={24} className="timer--container-time">
+          <span className="timer--container-time-date">{myTime}</span>
+          <br/>
+          <span className="timer--container-time-elapsed">Duration: {myTimeElapsed}</span>
+        </Col>
       </Row>
       <Row>
         <Col span={24} className="timer--container-actions">
