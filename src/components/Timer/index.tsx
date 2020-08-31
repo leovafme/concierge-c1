c4c1afafad4e782616f9c82fcf9c8a2c4f99d0b5
lavalencia9029 <lavalencia9029@misena.edu.co>
2020-08-31 15:35:30 -0500

    added ant and css class

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,20 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import { Button } from 'antd';
+import { Row, Col } from 'antd';
+import './index.css'
 
 function Timer() {
-  const [myTime, setMyTime] = useState(Date());
+  const [myTime, setMyTime] = useState("00:00");
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setMyTime(Date());
+      const date = moment().format('mm:ss');
+      setMyTime(`${date}`);
     }, 1000);
 
     return () => clearTimeout(timer);
   }, [myTime]);
 
   return (
-    <div>
-      {myTime}
-    </div>
+    <>
+      <Row>
+        <Col span={24} className="timer--container-time">{myTime}</Col>
+      </Row>
+      <Row>
+        <Col span={24} className="timer--container-actions">
+          <Button type="primary">Start</Button>
+          {" "}
+          <Button>Stop</Button>
+          {" "}
+          <Button>Clean</Button>  
+        </Col>
+      </Row>
+    </>
   );
 }
 
